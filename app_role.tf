79777969da21b40fe975a27e0a3c2095124ed8e1
mustafa-hammer <mustafa.elhilo@hashicorp.com>
2024-08-16 15:05:42 -0700

    Wrong namespace

--- app_role.tf
+++ app_role.tf
@@ -1,15 +1,18 @@
 # Create admin policy in the root namespace
 resource "vault_policy" "read_only" {
-  name   = "read_only"
-  policy = file("${path.module}/policies/read_only.hcl")
+  namespace = var.namespace
+  name      = "read_only"
+  policy    = file("${path.module}/policies/read_only.hcl")
 }
 
 resource "vault_auth_backend" "approle" {
-  type = "approle"
-  path = "${var.team_name}-approle"
+  namespace = var.namespace
+  type      = "approle"
+  path      = "${var.team_name}-approle"
 }
 
 resource "vault_approle_auth_backend_role" "team_role_read_only" {
+  namespace  = var.namespace
   depends_on = [vault_auth_backend.approle, vault_policy.read_only]
 
   backend        = vault_auth_backend.approle.path
